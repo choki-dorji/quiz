8b2aceefa2afa797efe78e87a2e82993f930b0ba
choki-dorji <62417033+choki-dorji@users.noreply.github.com>
2022-08-28 17:08:03 +0600

    Update settings.py

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -17,7 +17,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-STATIC_DIR=os.path.join(BASE_DIR,'static')
+# STATIC_DIR=os.path.join(BASE_DIR,'static')
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
@@ -129,9 +129,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[
-STATIC_DIR,
- ]
+# STATICFILES_DIRS=[
+# STATIC_DIR,
+#  ]
 
 
 LOGIN_REDIRECT_URL='/afterlogin'
